8bcca90bc140274f8bac98a7944a681322f0c524
yakuphantonintshi <yakupha58756@gmail.com>
2024-05-17 16:24:34 +0200

    Add File

---
+++ scripts/code.js
@@ -0,0 +1,30 @@
+//Question 1
+let firstName = "Yakupha"
+console.log(typeof firstName)
+
+//Question 2
+let numb1 = "Yakupha"
+console.log(typeof numb1)
+//the type of the value did not change as it is still a string even though I assigned it to an number variable
+
+//Question 3
+let numb2 = 78;
+console.log(typeof numb2)
+
+let numb3 = "3.14"
+console.log(typeof numb3);
+
+let lastName = "Ntonintshi"
+console.log(typeof lastName);
+
+let numb4 = 49;
+console.log(typeof NaN);
+
+let numb5 = 79;
+console.log(typeof false);
+
+let numb6 = 100;
+console.log(typeof [numb1, numb2, numb3, numb4, numb5]);
+
+let firstNames = "yakupha"
+console.log(typeof n);
